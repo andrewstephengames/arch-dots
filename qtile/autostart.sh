9296d345f6bd42ad6ad6edcb843cb3e463e19cb2
andrewstephengames <andrewstephengames999@gmail.com>
2021-08-25 14:17:06 +0300

    Adding the dots

---
+++ qtile/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+picom --experimental-backends &
+#appimagelauncherd &
+lxsession &
+dunst &
+redshift &
+unclutter --timeout 5 &
+nitrogen --restore &
+flameshot &
+pcmanfm -d &
+kdeconnect-indicator &
+transmission-daemon &
+xset -dpms &
+xset s off &
+xset -b &
+#brave &
