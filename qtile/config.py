909e57ce55cbdb77f540e5049e5a61253b698ae6
andrewstephengames <andrewstephengames999@gmail.com>
2021-09-07 17:17:05 +0300

    Added qtile media buttons, improved rofi

--- qtile/config.py
+++ qtile/config.py
@@ -117,6 +117,12 @@ keys = [
         desc="Toggle minimize *window"),
     Key([mod], "x", lazy.spawn("scripts/shell/cpu"),
         desc="Send notification with top 5 most intensive processes"),
+    Key(["control", "shift"], "space", lazy.spawn("mpvc -p"),
+        desc="Pause mpv music"),
+    Key(["control", "shift"], "x", lazy.spawn("mpvc prev"),
+        desc="Prev mpv music"),
+    Key(["control", "shift"], "c", lazy.spawn("mpvc next"),
+        desc="Next mpv music"),
 #    Key([mod], "d", lazy.spawn("deadbeef"),
 #        desc="Launch Deadbeef"),
 ]
@@ -203,11 +209,11 @@ screens = [
                     custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
                 ),
 # song name
-                widget.GenPollText(
-                    func=lambda: subprocess.check_output("/home/andrew/scripts/shell/mpvctitle").decode("utf-8"),
-                    update_interval=1,
-                    max_chars=32,
-                ),
+#                widget.GenPollText(
+#                    func=lambda: subprocess.check_output("/home/andrew/scripts/shell/mpvctitle").decode("utf-8"),
+#                    update_interval=1,
+#                    max_chars=32,
+#                ),
                 widget.Image(
                     filename='~/.config/qtile/icons/previous-button.png',
                     mouse_callbacks={
@@ -233,22 +239,23 @@ screens = [
                                     },
                 ),
 #song duration
-                widget.GenPollText(
-                    func=lambda: subprocess.check_output("/home/andrew/scripts/shell/mpvcduration").decode("utf-8"),
-                    update_interval=0.5,
-                ),
-                widget.Spacer(
-                ),
-#                widget.TaskList(
-#                    mouse_callbacks={'Button2': lambda: qtile.cmd_spawn('wmctrl -c :ACTIVE:')},
-#                    txt_floating='',
-#                    txt_minimized='',
-#                    borderwidth=0,
-#                    spacing=10,
-#                    margin=-3,
+#                widget.GenPollText(
+#                    func=lambda: subprocess.check_output("/home/andrew/scripts/shell/mpvcduration").decode("utf-8"),
+#                    update_interval=0.5,
 #                ),
+                widget.TaskList(
+                    mouse_callbacks={'Button2': lambda: qtile.cmd_spawn('wmctrl -c :ACTIVE:')},
+                    txt_floating='',
+                    txt_minimized='',
+                    borderwidth=0,
+                    spacing=10,
+                    margin=-3,
+                ),
 #                widget.WindowName(
-#                    mouse_callbacks={'Button2': lambda: qtile.cmd_spawn('wmctrl -c :ACTIVE:')},
+#                    mouse_callbacks={
+#                                        'Button1': lambda: qtile.lazy.spawn.toggle_minimize(),
+#                                        'Button2': lambda: qtile.cmd_spawn('wmctrl -c :ACTIVE:')
+#                                    },
 #                    txt_floating=' ',
 #                ),
                 widget.TextBox(text=' '),
